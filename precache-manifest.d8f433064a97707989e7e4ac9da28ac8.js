45d7057d946fc3accd3f605ca1fd1418b90577e1
TheVinh <binkool281132442@gmail.com>
2020-09-17 18:16:07 +0700

    Updates

---
+++ precache-manifest.d8f433064a97707989e7e4ac9da28ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429a682d64cfbf78f0a05cef39e46bdc",
+    "url": "/api-picture/index.html"
+  },
+  {
+    "revision": "7846fee6f6db1c108df8",
+    "url": "/api-picture/static/css/main.3da5a672.chunk.css"
+  },
+  {
+    "revision": "0570e3fb2af07b63a354",
+    "url": "/api-picture/static/js/2.5054c8f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api-picture/static/js/2.5054c8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7846fee6f6db1c108df8",
+    "url": "/api-picture/static/js/main.f50eadbb.chunk.js"
+  },
+  {
+    "revision": "e77588f9f844c8246ba1",
+    "url": "/api-picture/static/js/runtime-main.9c23e22c.js"
+  }
+]);
